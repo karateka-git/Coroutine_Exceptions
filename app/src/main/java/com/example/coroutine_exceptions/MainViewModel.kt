20b498c3ae60b771c8c7351610f6e1bf0c249df5
compadre <zvvlad2@gmail.com>
2023-12-11 15:03:48 +0600

    Добавление CoroutineExceptionHandler

--- app/src/main/java/com/example/coroutine_exceptions/MainViewModel.kt
+++ app/src/main/java/com/example/coroutine_exceptions/MainViewModel.kt
@@ -3,27 +3,28 @@ package com.example.coroutine_exceptions
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     private val mainRepository = MainRepository()
     val list: MutableLiveData<List<String>> = MutableLiveData(listOf())
 
+    private val ceh = CoroutineExceptionHandler { coroutineContext, throwable ->
+        list.setValue(list.value?.plus(throwable.toString()))
+    }
+
     fun getSimpleException() {
-        viewModelScope.launch {
-            try {
-                launch {
-                    mainRepository.getException().also {
-                        list.setValue(list.value?.plus(it))
-                    }
+        viewModelScope.launch(ceh) {
+            launch {
+                mainRepository.getException().also {
+                    list.setValue(list.value?.plus(it))
                 }
-                launch {
-                    mainRepository.getData().also {
-                        list.setValue(list.value?.plus(it))
-                    }
+            }
+            launch {
+                mainRepository.getData().also {
+                    list.setValue(list.value?.plus(it))
                 }
-            } catch (e: Throwable) {
-                list.setValue(list.value?.plus(e.toString()))
             }
         }
     }
