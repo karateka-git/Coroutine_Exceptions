4e439c9f80e6facc263967956dc58884a4c3942b
compadre <zvvlad2@gmail.com>
2023-12-11 14:45:42 +0600

    Structured Concurrency, ведущий к DiagnosticCoroutineContextException

--- app/src/main/java/com/example/coroutine_exceptions/MainViewModel.kt
+++ app/src/main/java/com/example/coroutine_exceptions/MainViewModel.kt
@@ -4,35 +4,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import java.util.concurrent.CancellationException
 
 class MainViewModel : ViewModel() {
     private val mainRepository = MainRepository()
     val list: MutableLiveData<List<String>> = MutableLiveData(listOf())
 
     fun getSimpleException() {
-        val job = viewModelScope.launch {
+        viewModelScope.launch {
             try {
-                mainRepository.getException().also {
-                    list.setValue(list.value?.plus(it))
+                launch {
+                    mainRepository.getException().also {
+                        list.setValue(list.value?.plus(it))
+                    }
                 }
-            } catch (e: Throwable) {
-                if (e is CancellationException) {
-                    throw e
-                } else {
-                    list.setValue(list.value?.plus(e.toString()))
-                }
-            }
-            try {
-                mainRepository.getException().also {
-                    list.setValue(list.value?.plus(it))
+                launch {
+                    mainRepository.getData().also {
+                        list.setValue(list.value?.plus(it))
+                    }
                 }
             } catch (e: Throwable) {
                 list.setValue(list.value?.plus(e.toString()))
             }
-        }
-        viewModelScope.launch {
-            job.cancel(CancellationException("Galya, otmena!"))
         }
     }
 }
