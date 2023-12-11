e1b823122f91268b7e861efee82e6d290eba1db9
compadre <zvvlad2@gmail.com>
2023-12-11 14:25:15 +0600

    try_catch при CancellationException

--- app/src/main/java/com/example/coroutine_exceptions/MainViewModel.kt
+++ app/src/main/java/com/example/coroutine_exceptions/MainViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import java.util.concurrent.CancellationException
 
 class MainViewModel : ViewModel() {
     private val mainRepository = MainRepository()
     val list: MutableLiveData<List<String>> = MutableLiveData(listOf())
 
     fun getSimpleException() {
-        viewModelScope.launch {
+        val job = viewModelScope.launch {
             try {
                 mainRepository.getException().also {
                     list.setValue(list.value?.plus(it))
@@ -18,6 +19,16 @@ class MainViewModel : ViewModel() {
             } catch (e: Throwable) {
                 list.setValue(list.value?.plus(e.toString()))
             }
+            try {
+                mainRepository.getException().also {
+                    list.setValue(list.value?.plus(it))
+                }
+            } catch (e: Throwable) {
+                list.setValue(list.value?.plus(e.toString()))
+            }
+        }
+        viewModelScope.launch {
+            job.cancel(CancellationException("Galya, otmena!"))
         }
     }
 }
